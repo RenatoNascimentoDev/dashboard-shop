45e429578562bdb73622bc9ed6ac798a0880613a
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-08 20:05:04 +0000

    Página: Cadastro de restaurante

--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -6,21 +6,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast } from "sonner";
+import { Link } from "react-router-dom";
 
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z.string().email(),
 });
 
-type SignInForm = z.infer<typeof signInForm>;
+type SignUpForm = z.infer<typeof signUpForm>;
 
 export function SignIn() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>();
+  } = useForm<SignUpForm>();
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       console.log(data);
 
@@ -29,7 +30,7 @@ export function SignIn() {
       toast.success("Enviamos um link de autenticação para seu e-mail.", {
         action: {
           label: "Reenviar",
-          onClick: () => handleSignIn(data),
+          onClick: () => handleSignUp(data),
         },
       });
     } catch {
@@ -41,6 +42,11 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild>
+          <Link to="/sign-up" className="absolute right-4 top-8">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="w-[350px] flex flex-col justify-center gap-6"></div>
         <div className="flex flex-col gap-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
@@ -51,7 +57,7 @@ export function SignIn() {
           </p>
         </div>
 
-        <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+        <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Seu e-mail</Label>
             <Input id="email" type="e-mail" {...register("email")} />

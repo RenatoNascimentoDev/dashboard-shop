3887c610a022782c32da8bfe006f9c077507cc05
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-09 01:41:40 +0000

    Ajustando rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,28 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sing-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
-import { SignUp } from "./pages/auth/sign-up";
+import { Dashboard } from "./pages/app/dashboard";
 import { Orders } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
     children: [
-      { path: "/", element: <Dashboard /> },
-      { path: "/orders", element: <Orders /> },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/orders",
+        element: <Orders />,
+      },
     ],
   },
-
   {
     path: "/",
     element: <AuthLayout />,
     children: [
-      { path: "/sign-in", element: <SignIn /> },
-      { path: "/sign-up", element: <SignUp /> },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
+      },
     ],
   },
 ]);

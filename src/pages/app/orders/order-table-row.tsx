71972e50c9f0524f405969aa3a859940f11a6de0
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-10 13:45:14 +0000

    Componente: Detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,17 +1,27 @@
-import { Button } from "@/components/ui/button";
-import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
 
-// export interface OrderTableRowProps {}
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+import { OrderDetails } from "./order-details";
+
+// interface OrderTableRowProps {}
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         821e78f7asdhdf128h
@@ -26,13 +36,13 @@ export function OrderTableRow() {
       <TableCell className="font-medium">Diego Schell Fernandes</TableCell>
       <TableCell className="font-medium">R$ 149,90</TableCell>
       <TableCell>
-        <Button variant="outline">
+        <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
           Aprovar
         </Button>
       </TableCell>
       <TableCell>
-        <Button variant="ghost">
+        <Button variant="ghost" size="xs">
           <X className="mr-2 h-3 w-3" />
           Cancelar
         </Button>
